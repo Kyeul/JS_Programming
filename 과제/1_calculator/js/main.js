cab838e8047f6d8b421fdfcf537af27146de0218
kyeul <gyul611@gmail.com>
2020-05-01 10:38:04 +0900

    2020-05-01

--- 과제/1_calculator/js/main.js
+++ 과제/1_calculator/js/main.js
@@ -6,22 +6,8 @@ result = 0;
 check_btn = 0;
 check_frac = 0;
 
-// 사칙연산자 함수
-function sum(num, now){
-	return num + now;
-}
-function diff(num, now){
-	return num - now;
-}
-function mult(num, now){
-	return num * now;
-}
-function div(num, now){
-	return num / now;
-}
-
 // 수식 배열을 보여주는 함수
-function arrFormula(arr_frml){
+function showFormula(arr_frml){
 	var prnt_frml =''
 	for (i=0; i<arr_frml.length; i++){
 				prnt_frml += arr_frml[i]+' ';	
@@ -41,55 +27,85 @@ function stringToInt(value){
 }
 
 // 사용자가 버튼을 클릭하면 함수 실행
-function get_btn(btn, mode){
+function getBtn(btn, mode){
 
 	var crnt = document.getElementById('crnt-nmbr');
 	var frml = document.getElementById('formula');
 
+
 	// 숫자가 입력되면 실행
 	if (mode == 0){
-		if(check_btn==1 || crnt.value==0){
+		if(crnt.value.indexOf('.',0) != 1 && (check_btn==1 || crnt.value.indexOf(0,0) == 0 )){
 			crnt.value = null;
 		}
 		crnt.value = numberFormat(stringToInt(crnt.value)+btn);
 		check_btn = 0;
 	}
 
-	else if(mode == 3){
-		if(btn == "."){
-			if(check_btn == 1 || crnt.value == 0){
-				crnt.value = null;
-				crnt.value = 0 +'.'
-			}else if (check_frac == 0) {
-				crnt.value = crnt.value + '.'
-				check_frac = 1;
-			}
-		}
-	}
-
 	// 연산자가 입력되면 실행
 	else if (mode == 1){
-		var operator = btn;
+		// 최초의 입력 값은 result 변수에 들어간다.
+		if (result == 0){
+			result = stringToInt(crnt.value);
+		}
 		
 		//  연산자가 연속 두 번 눌렸을 경우 
 		if (check_btn == 1){
 			var crnt_1 = crnt.value.substring(0, crnt.value.length) 
 			arr_frml.pop()
-			frml.value = arrFormula(arr_frml) + btn; // 기존 연산자를 지우고 새로운 연산자로 대체
+			frml.value = showFormula(arr_frml) + btn; // 기존 연산자를 지우고 새로운 연산자로 대체
 			arr_frml.push(btn)
 		}
 
 		// 연산자가 처음 눌렸을 경우
 		else{
 			num = stringToInt(crnt.value);
-			arr_frml.push(num)
-			arr_frml.push(btn)
-			frml.value = arrFormula(arr_frml)
+			if (num.indexOf('.',0) == num.length-1){
+				num = num.substring(0, num.length-1);
+			}
+
+			arr_frml.push(num);
+			// 사칙연산 수행
+			if(arr_frml.length>2){
+				result = eval(result + arr[arr.length-2] + arr[arr.length-1]);
+			}
+			arr_frml.push(btn);
+			frml.value = showFormula(arr_frml);
+			crnt.value = result;
+			if(btn == '='){
+					num = 0;
+					result = 0;
+					check_btn = 0;
+					check_frac = 0;
+					arr_frml.length = 0;
+				}
 		}
 		check_btn = 1; //연산자 체크
+		check_frac = 0;	//소수점 체크 초기화
 
 	}
-	// 계산기 reset
+	// 부가 연산자 ('.', '-/+', '=')
+	else if(mode == 3){
+		if(btn == "."){
+			if(check_btn == 1 || crnt.value == 0){
+				crnt.value = null;
+				crnt.value = 0 +'.'
+			}
+			if (check_frac == 0 && crnt.value != 0) {
+				crnt.value = crnt.value + '.'
+				
+			}
+			check_frac = 1;
+		}else if(btn =='nega'){
+			if(crnt.value.indexOf('-', 0 ) == 0){
+				crnt.value = crnt.value.substring(1, crnt.value.length);
+			}else{
+				crnt.value = '-' + crnt.value;
+			}
+		}
+	}
+
+	// reset
 	else if(mode == 2){
 		// 전체 초기화
 		if(btn == 'all'){
@@ -98,18 +114,23 @@ function get_btn(btn, mode){
 			check_btn = 0;
 			check_frac = 0;
 			arr_frml.length = 0;
-			frml.value = arrFormula(arr_frml);
+			frml.value = showFormula(arr_frml);
 			crnt.value = 0;
-
 		}
 		// 입력 숫자 초기화
 		else if(btn == 'crnt'){
+			check_frac = 0;
 			crnt.value = 0;
 		}
 		// 입력 숫자의 마지막 숫자 제거
 		else{
-
-			crnt.value = numberFormat(stringToInt(crnt.value.substring(0, crnt.value.length-1)))
+			crnt.value = numberFormat(stringToInt(crnt.value.substring(0, crnt.value.length-1)));
+			if(crnt.value.length == 0){
+				crnt.value = 0
+			}
+			if (crnt.value.indexOf('.',0) == -1){
+				check_frac = 0;
+			}
 		}
 	}
 }
